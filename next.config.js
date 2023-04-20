1a0239d7721bb09eebcd050f6945b142bff5fda4
Anh Chun <wanbicoi123@gmail.com>
2023-04-20 16:49:05 +0700

    Remove some unused packages and ready to fork

--- next.config.js
+++ next.config.js
@@ -1,20 +1,49 @@
-const withPWA = require('next-pwa');
+module.exports = async (phase, { defaultConfig }) => {
+  const nextConfig = {
+    // future: { webpack5: true },
+    // pwa: {
+    //   dest: "public",
+    //   disable: process.env.NODE_ENV === "development",
+    // },
+    env: {
+      NEXT_PUBLIC_SUPABASE_URL: "https://mcqzpfkknjhuyegcmibg.supabase.co",
+      NEXT_PUBLIC_SUPABASE_ANON_KEY:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jcXpwZmtrbmpodXllZ2NtaWJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5NzczNTUsImV4cCI6MTk5NzU1MzM1NX0.Z3ixgyb5edVmTWeYLGb_BVZThb2GoGxPT1Iy5l4sHzM",
+      siteTitle: "Doggy Stickers",
+      siteDescription: "Get some Doggy Stickers!",
+      siteKeywords: "dog, stickers, fun",
+      siteUrl: "https://www.doggystickers.xyz",
+      siteImagePreviewUrl: "/images/main.jpg",
+      twitterHandle: "@deepwhitman",
+    },
+    images: {
+      domains: ["cdn.shopify.com"],
+    },
+  };
+  return nextConfig;
+};
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+// });
 
-module.exports = withPWA({
-  future: { webpack5: true },
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
-  env: {
-    siteTitle: 'Doggy Stickers',
-    siteDescription: 'Get some Doggy Stickers!',
-    siteKeywords: 'dog, stickers, fun',
-    siteUrl: 'https://www.doggystickers.xyz',
-    siteImagePreviewUrl: '/images/main.jpg',
-    twitterHandle: '@deepwhitman'
-  },
-  images: {
-    domains: ['cdn.shopify.com'],
-  },
-})
+// module.exports = withPWA({
+//   // future: { webpack5: true },
+//   // pwa: {
+//   //   dest: "public",
+//   //   disable: process.env.NODE_ENV === "development",
+//   // },
+//   env: {
+//     NEXT_PUBLIC_SUPABASE_URL: "https://mcqzpfkknjhuyegcmibg.supabase.co",
+//     NEXT_PUBLIC_SUPABASE_ANON_KEY:
+//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jcXpwZmtrbmpodXllZ2NtaWJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5NzczNTUsImV4cCI6MTk5NzU1MzM1NX0.Z3ixgyb5edVmTWeYLGb_BVZThb2GoGxPT1Iy5l4sHzM",
+//     siteTitle: "Doggy Stickers",
+//     siteDescription: "Get some Doggy Stickers!",
+//     siteKeywords: "dog, stickers, fun",
+//     siteUrl: "https://www.doggystickers.xyz",
+//     siteImagePreviewUrl: "/images/main.jpg",
+//     twitterHandle: "@deepwhitman",
+//   },
+//   images: {
+//     domains: ["cdn.shopify.com"],
+//   },
+// });
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,23 @@
-import StoreHeading from '@/components/StoreHeading'
-import ProductListings from '@/components/ProductListings'
-import { getAllProductsInCollection } from '@/lib/shopify'
+import StoreHeading from "@/components/StoreHeading";
+import ProductListings from "@/components/ProductListings";
+import { getAllProductsInCollection } from "@/lib/superbase";
 
 function IndexPage({ products }) {
-
   return (
     <div className="mx-auto max-w-6xl">
       <StoreHeading />
-      <ProductListings products={products} />      
+      <ProductListings products={products} />
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const products = await getAllProductsInCollection()
-
+  const products = await getAllProductsInCollection();
   return {
     props: {
-      products
+      products,
     },
-  }
+  };
 }
 
-export default IndexPage
+export default IndexPage;
